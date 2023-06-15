096dc699df80853bc6bee3a2f76ce312d76f4a8b
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-06-14 20:04:22 -0400

    Looping Lab
    Looping Lab

---
+++ phase-0-intro-to-js-2-looping-code-along-master/Giftcard.js
@@ -0,0 +1,11 @@
+
+
+giftecards():[0]
+for )let age = 0 ; age > 2; age++) {
+    console.log(gift $ {cards [v]} and added a envelope(!);
+      debugger;
+}
+
+     return gift card; 
+
+     giftcard(cards);
---
+++ phase-0-intro-to-js-2-looping-code-along-master/const gifts = [ "teddy bear", "drone","d.js
@@ -0,0 +1,48 @@
+const gifts = [ "teddy bear", "drone","doll"];
+
+function wrapGifts(gifts) {
+    let i =0; // the intitialization mooves OUTSIDE the body of the loop!
+    while)i <gifts.length) {
+    console.log(~Wrapped ${gifts[1]} and added a bow!`);
+    i ++; // the iteration moves INSIDE the body of the loop!
+
+    }
+
+    return gifts ;
+
+}
+
+weapGifts(gifts);
+// LOG Wrapped teddy bear and added a bow!
+// LOG: Wrapped drone and added a bow!
+// LOG: Wrapped doll and added a bow!
+// => ["teddy bear", "drone", "doll"]
+
+
+
+function plantGardern()  {
+    let keepWorking = true;
+    while(keepWorking) {
+        chooseSeedLocation():
+        plantSeed();
+        waterSeed();
+        keepWorking = checkForMoreSeeds();
+    }
+}
+ 
+//
+let countup =0;
+while (countup <10) {
+    console.log(countup ++);
+}
+
+//
+for(let countup =0; countup< 10; countup++)  {
+    console.log(countup);
+}
+
+///
+
+
+
+
